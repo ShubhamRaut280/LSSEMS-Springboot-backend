c2120b73226328eb69234457cec50239a6c4fd0d
Shubham Raut <124999374+ShubhamRaut280@users.noreply.github.com>
2024-11-17 19:15:29 +0530

    initial

---
+++ src/main/java/com/shubham/localservices/LocalServicesApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.localservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubham/localservices/LocalServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.localservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
